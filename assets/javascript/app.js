5f637ec6a37404a810738a696869181806ef35eb
Chris Parolini <chris.parolini@chatid.com>
2017-01-07 09:57:13 -0600

    changing back to https

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,6 +15,10 @@ var hasAlcohol = '';
 //holds the current clickable drinks on the page
 var currentDrinks = '';
 
+$('.logo').on("click", function() {
+    location.reload();
+});
+
 // Event listener for all drop down elements except the non Alcoholic drinks (special case)
 $(".mixMenu").on("click", function() {
     // In this case, the "this" keyword refers to the dropdown option that was clicked
@@ -27,7 +31,7 @@ $(".mixMenu").on("click", function() {
 
     // Constructing a URL to search cocktail db - the search term is filled by the
     //drop down that was selected
-    queryURL = "http://www.thecocktaildb.com/api/json/v1/6526/search.php?s=" + dropDownDrink;
+    queryURL = "https://www.thecocktaildb.com/api/json/v1/6526/search.php?s=" + dropDownDrink;
 
     //uses the defulat query
     hasAlcohol = 'alcoholic';
@@ -47,7 +51,7 @@ $(".nonAlcoholic").on("click", function() {
 
     // Constructing a URL to search cocktail db - the search term is filled by the
     //drop down that was selected
-    queryURL = "http://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic";
+    queryURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic";
 
     //special search case, this will cause a different function to be called
     //when a nonAlcoholic drink is clicked
@@ -70,7 +74,7 @@ $(".search").on("click", function() {
     var searchFor = $('.drinkForm').val();
 
     // Constructing a URL to search cocktail db
-    var queryURL = "http://www.thecocktaildb.com/api/json/v1/6526/search.php?s=" + searchFor;
+    var queryURL = "https://www.thecocktaildb.com/api/json/v1/6526/search.php?s=" + searchFor;
 
     //default search, this works for any type of drink (alcoholic or not)
     //because the url we use above
@@ -91,7 +95,7 @@ $("#search").on("click", function() {
     drinkName = searchFor.toLowerCase().replace(/ /g, "-");
     console.log(drinkName);
     // Constructing a URL to search cocktail db
-    var queryURL2 = "http://api.malt.io/v1/public/recipes?detail=true&slugs=" + drinkName;
+    var queryURL2 = "https://api.malt.io/v1/public/recipes?detail=true&slugs=" + drinkName;
 
     // Performing our AJAX GET request
     ajaxList2(queryURL2);
@@ -120,7 +124,7 @@ $(".beerMenu").on("click", function() {
     dropDownDrink = this.outerText;
     drinkName = dropDownDrink.toLowerCase().replace(/ /g, "-");
     // Constructing a URL to search cocktail db
-    queryURL2 = "http://api.malt.io/v1/public/recipes?detail=true&slugs=" + drinkName;
+    queryURL2 = "https://api.malt.io/v1/public/recipes?detail=true&slugs=" + drinkName;
     if (dropDownDrink === "American Pale Ale") {
         $(".drinks").html("<h1 class='beerClass'>" + dropDownDrink + "</h1>");
         $(".drinks").append("<p class='time'>Total time until able to drink 28 days!</p>" + "<hr>");
@@ -161,7 +165,7 @@ $(".drinks").on("click", '.beerClass', function() {
     // dropDownDrink = this.outerText;
     // drinkName = dropDownDrink.toLowerCase().replace(/ /g, "-");
     // // Constructing a URL to search cocktail db
-    // url = "http://api.malt.io/v1/public/recipes?detail=true&slugs=" + drinkName;
+    // url = "https://api.malt.io/v1/public/recipes?detail=true&slugs=" + drinkName;
     // console.log(url);
 
     callModal();
@@ -195,7 +199,7 @@ $(".drinks").on("click", '.nonAlcoholic', function() {
     drinkSearch = drinkSearch.replace(/'/g, "&#39");
 
     // Constructing a URL to search cocktail db, the term comes from the clicked html
-    queryURL = "http://www.thecocktaildb.com/api/json/v1/6526/search.php?s=" + drinkSearch;
+    queryURL = "https://www.thecocktaildb.com/api/json/v1/6526/search.php?s=" + drinkSearch;
 
     //calls the popup and empties the old values
     callModal();
@@ -287,13 +291,13 @@ var ajaxList2 = function(queryURL2) {
             // $('.drinks').append("<li><a class = 'drinkRecipe'>" + response[i].data.name + "</a></li>");
             // $(".modal-body").html("<img src='assets/image/" + dropDownDrink + ".jpg'>");
             if (dropDownDrink === 'American Pale Ale') {
-                $(".modal-body").html("<iframe width='420' height='315'src='http://www.youtube.com/embed/ZCy1cXeHDTQ'>" + "</iframe>");
+                $(".modal-body").html("<iframe width='420' height='315'src='https://www.youtube.com/embed/ZCy1cXeHDTQ'>" + "</iframe>");
             } else if (dropDownDrink === 'Dark Beer') {
-                $(".modal-body").html("<iframe width='420' height='315'src='http://www.youtube.com/embed/SrPi4y9lOmc'>" + "</iframe>");
+                $(".modal-body").html("<iframe width='420' height='315'src='https://www.youtube.com/embed/SrPi4y9lOmc'>" + "</iframe>");
             } else if (dropDownDrink === 'Hefeweizen') {
-                $(".modal-body").html("<iframe width='420' height='315'src='http://www.youtube.com/embed/EPfCTNiEggc'>" + "</iframe>");
+                $(".modal-body").html("<iframe width='420' height='315'src='https://www.youtube.com/embed/EPfCTNiEggc'>" + "</iframe>");
             } else if (dropDownDrink === 'Irish Red') {
-                $(".modal-body").html("<iframe width='420' height='315'src='http://www.youtube.com/embed/sTkwdIq7KSw'>" + "</iframe>");
+                $(".modal-body").html("<iframe width='420' height='315'src='https://www.youtube.com/embed/sTkwdIq7KSw'>" + "</iframe>");
             } else {
                 return false;
             }
@@ -437,7 +441,7 @@ var ajaxDrink = function(queryURL) {
 var config = {
     apiKey: "AIzaSyAWsHg3xzxLY3hT-WfHJJtJL3JmLqW7R8c",
     authDomain: "hoppify-1bdea.firebaseapp.com",
-    databaseURL: "http://hoppify-1bdea.firebaseio.com",
+    databaseURL: "https://hoppify-1bdea.firebaseio.com",
     storageBucket: "hoppify-1bdea.appspot.com",
     messagingSenderId: "939603520664"
 };
