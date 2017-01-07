5f4f2b67f44f8e562c784420d83e148b3ee26bdb
Vincent Gonzalez <elpollo_atx44@yahoo.com>
2017-01-06 20:21:41 -0600

    hoppify

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -157,7 +157,7 @@ $('#license').on('click', function(){
 	$('.drinkRecipe').empty();
 	$('.info').empty();
 //add a image link and paragraph
-	$('.info').html("<a href='https://txbartendinglicense.com/web/index.php?siteid=61&pageid=339'>"+'<img src="assets/image/tabc.jpg"'+"</a>");
+	$('.info').html("<a href='https://txbartendinglicense.com/web/index.php?siteid=61&pageid=339' target='_blank'>"+'<img src="assets/image/tabc.jpg"'+"</a>");
 	$('.drinks').html("<p>'Learn How to be a Great Bartender!<br> This Bartender Mixology course is a great way to learn how to make hundreds of different drinks.It's a self-paced course that rated at 40 hours and contains all the information you need to know to become a great bartender and make better tip money!  This is a great certification to have on your resume, as it demonstrates that you have taken extensive training and are familiar with the drinks, terminology, and equipment involved with the bar or restaurant field of work. Click on image to get more information and GOOD LUCK!'</p>")
 })
 //click on about tab
